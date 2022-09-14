eb5e2ffea592e36df8067214326a43ed9a1cc5e5
elsa.giana <elsagiana18@gmail.com>
2022-09-14 23:00:33 +0700

    Implement MVT

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_catalog(request):
+    data_catalog = CatalogItem.objects.all()
+    context = {
+        'list_catalog': data_catalog,
+        'name': 'Elsa Giana Abigail Sitompul',
+        'npm': '2106707252'
+    }
+    return render(request, "katalog.html", context)
