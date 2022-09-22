de39d7e0926d695186b6785bd085b222cb90d2c9
elsa.giana <elsagiana18@gmail.com>
2022-09-22 10:27:02 +0700

    update url

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -4,7 +4,7 @@ from mywatchlist.views import show_mywatchlist, show_mywatchlist_xml, show_mywat
 app_name = 'mywatchlist'
 
 urlpatterns = [
-    path('', show_mywatchlist, name='show_mywatchlist'),
+    path('html/', show_mywatchlist, name='show_mywatchlist'),
     path('xml/', show_mywatchlist_xml, name='show_mywatchlist_xml'),
     path('json/', show_mywatchlist_json, name='show_mywatchlist_json'),
 ]
