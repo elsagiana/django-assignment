c6a95dff5f21dfc28488999934af5798ab57d652
elsa.giana <elsagiana18@gmail.com>
2022-09-22 11:43:38 +0700

    add unit test

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import resolve
 
 # Create your tests here.
+class MywatchlistTest(TestCase):
+    def test_mywatchlist_html_exists(self):
+        response = Client().get('https://django-assignment-elsa.herokuapp.com/mywatchlist/html/')
+        self.assertEqual(response.status_code,200)
+
+    def test_mywatchlist_xml_exists(self):
+        response = Client().get('https://django-assignment-elsa.herokuapp.com/mywatchlist/xml/')
+        self.assertEqual(response.status_code,200)    
+
+    def test_mywatchlist_json_exists(self):
+        response = Client().get('https://django-assignment-elsa.herokuapp.com/mywatchlist/json/')
+        self.assertEqual(response.status_code,200)
